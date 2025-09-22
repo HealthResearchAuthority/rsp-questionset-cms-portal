0ae40bc9677f8cc34d89f5b67bfa7319f93e1a4c
bvasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-09-22 15:07:33 +0100

    Enable Azure Configuration for the CMS project (#18)

---
+++ Rsp.QuestionSetPortal/Configuration/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace Rsp.QuestionSetPortal.Configuration;
+
+public class AppSettings
+{
+    /// <summary>
+    /// Label to use when reading App Configuration from AzureAppConfiguration
+    /// </summary>
+    public const string ServiceLabel = "cms";
+
+    /// <summary>
+    /// Azure App Configuration settings
+    /// </summary>
+    public AzureAppConfigurations AzureAppConfiguration { get; set; } = null!;
+
+    public Uri? PortalUrl { get; set; }
+}
